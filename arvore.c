e23ffd62ab5b8fb9dca14feb5ba95cea48ce3969
glrabelo <43296212+glrabelo@users.noreply.github.com>
2018-11-10 18:07:34 -0200

    aggiornato

--- arvore.c
+++ arvore.c
@@ -8,14 +8,42 @@ struct _arvore {
   Candidato cand;
 };
 
-Arvore* arvore_inserir(Arvore *a, Candidato cand) {
-  //Implemente
+Arvore* arvore_inserir(Arvore *a, Candidato cand.numero) {
+	if(a == NULL) {
+		Arvore *novo = (Arvore*) malloc(sizeof(Arvore));
+		novo->cand = cand;
+		novo->esquerda = NULL;
+		novo->direita = NULL;
+	return novo;
+	}
+	if(cand.numero < a->cand.numero) {
+		a->esquerda = arv_inserir(a->esquerda, cand);
+	} else {
+		a->direita = arv_inserir(a->direita, cand);
+	}
+	return a;
 }
 
-Candidato* arvore_buscar(Arvore *a, int candNumero) {
-  //Implemente
+Candidato* arvore_buscar(Arvore *a, int cand.numero) {
+	if(a == NULL || a->cand.numero == cand.numero) {
+		return a;
+	}
+	if(cand.numero < a->cand.numero) {
+		return arv_buscar(a->esquerda, cand.numero);
+	} else {
+		return arv_buscar(a->direita, cand.numero);
+	}
 }
 
 void arvore_imprime_em_ordem(Arvore *a) {
-  //Implemente
+	if(a->direita != NULL) {
+		arv_imprimir(a->direita);
+	}
+	printf("%lf\n", a->cand.numero);
+	printf("%lf\n", a->cand.nome);
+	printf("lf\n", a->cand.cargo);
+	
+	if(a->esquerda != NULL) {
+		arv_imprimir(a->esquerda);
+	}
 }
