be4564fac26c02a35cd5a265b1ec4f9b36e83658
glrabelo <43296212+glrabelo@users.noreply.github.com>
2018-11-10 21:28:19 -0200

    aggiornato anche

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#include <locale.h>
 #include "arvore.h"
 
 Arvore* carregar_candidatos() {
   char linha[1024];
   FILE *arquivo = fopen("candidatos.csv", "r");
   
-  //Descartar cabeçalho do arquivo:
+  //Descartar cabeÃ§alho do arquivo:
   fgets(linha, 1024, arquivo);
   
-  //Número da coluna que contém cada informação (0 é a primeira):
+  //NÃºmero da coluna que contÃ©m cada informaÃ§Ã£o (0 Ã© a primeira):
   int campoNome = 18;
   int campoNumero = 16;
   int campoCargo = 14;
   int totalCampos = 57;
   
-  //Arvore que será preenchida;
+  //Arvore que serÃ¡ preenchida;
   Arvore *a = NULL;
   
   //Ler enquanto houver linhas:
@@ -34,7 +35,7 @@ Arvore* carregar_candidatos() {
     
     int i;
     for(i = 0; i < totalCampos; i++) {
-      //Verificar se o campo atual é algum dos que nos interessa:
+      //Verificar se o campo atual Ã© algum dos que nos interessa:
       if(i == campoNome) {
         strcpy(c.nome, token);
       } else if(i == campoNumero) {
@@ -43,42 +44,54 @@ Arvore* carregar_candidatos() {
         strcpy(c.cargo, token);
       }
       
-      //Avançar para o próximo token:
+      //AvanÃ§ar para o prÃ³ximo token:
       token = strtok(NULL, delimitadores);
     }
     
-    //Os campos do candidato foram preenchidos, vamos adicioná-lo à árvore:
+    //Os campos do candidato foram preenchidos, vamos adicionÃ¡-lo Ã  Ã¡rvore:
     a = arvore_inserir(a, c);
   }
   
-  //Retornar árvore construída:
+  //Retornar Ã¡rvore construÃ­da:
   return a;
 }
 
 void buscar_por_numero(Arvore *candidatos) {
-  //Implemente
+	
+	setlocale(LC_ALL, "Portuguese");
+	int candNumero;
+	printf("Digite o numero de seu candidato: ");
+	scanf("%d", &candNumero);
+	printf("\n");
+	if ((arvore_buscar(candidatos, candNumero))==NULL) {
+		printf("Candidato não registrado! \n\n");
+	}
+	//Implemente
 }
 
 void imprimir_todos(Arvore *candidatos) {
+
+	printf("\n");
+	arvore_imprime_em_ordem(candidatos);
   //Implemente
 }
 
 int main() {
-  Arvore *candidatos = carregar_candidatos();
-  
-  while(1) {
-    printf("O que deseja fazer?\n[1] Buscar candidato por numero\n[2] Listar candidatos em ordem de numero\n[3] Sair\n\n>> ");
-    int opcao;
-    scanf("%d", &opcao);
+	setlocale(LC_ALL, "Portuguese");
+	Arvore *candidatos = carregar_candidatos();
+	while(1) {
+	    printf("O que deseja fazer?\n[1] Procurar candidato pelo numero de canditadura\n[2] Mostrar candidatos em ordem numerica\n[3] Sair\n\n>> ");
+	    int opcao;
+	    scanf("%d", &opcao);
     
-    if(opcao == 1) {
-      buscar_por_numero(candidatos);
-    } else if(opcao == 2) {
-      imprimir_todos(candidatos);
-    } else if(opcao == 3) {
-      return 0;
-    } else {
-      printf("Opcao invalida!\n");
-    }
-  }
+	    if(opcao == 1) {
+	      buscar_por_numero(candidatos);
+	    } else if(opcao == 2) {
+	      imprimir_todos(candidatos);
+	    } else if(opcao == 3) {
+	      return 0;
+	    } else {
+	      printf("Opção invalida!\n");
+    	}
+  	}
 }
